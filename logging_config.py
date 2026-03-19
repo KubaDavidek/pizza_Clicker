03a093b6a563fabbc00797328e2d8098624d58ae
begoslav <jirik.janousek1@gmail.com>
2026-03-19 13:45:27 +0100

    Vercel fix

--- logging_config.py
+++ logging_config.py
@@ -4,10 +4,11 @@ from logging.handlers import RotatingFileHandler
 
 
 def configure_logging(app):
-    log_file = os.path.join(os.path.dirname(__file__), 'server.log')
-    handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=3, encoding='utf-8')
-    handler.setLevel(logging.ERROR)
-    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-
     app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
+
+    if not os.getenv('VERCEL'):
+        log_file = os.path.join(os.path.dirname(__file__), 'server.log')
+        handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=3, encoding='utf-8')
+        handler.setLevel(logging.ERROR)
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+        app.logger.addHandler(handler)
